508f1a4733a75a72da754837ed4b1b062953688d
Fahri563 <fahriirfandi564@gmail.com>
2026-09-17 21:35:46 +0700

    pertemuan 3

---
+++ app/src/main/java/com/pemmob/Domge/HomeActivity.kt
@@ -0,0 +1,43 @@
+package com.pemmob.Domge
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.pemmob.Domge.ui.screen.DaftarProductScreen
+import com.pemmob.Domge.ui.theme.JualanTheme
+
+class HomeActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            JualanTheme {
+                DaftarProductScreen()
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting2(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    JualanTheme {
+        Greeting2("Android")
+    }
+}
